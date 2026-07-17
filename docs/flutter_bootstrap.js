059495dfd22f0b36bc1512a7805cb62193f2340a
manahi ali <shreefjamela@gmail.com>
2026-07-17 13:10:55 +0300

    feat: add app icons

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"42d3d75a56efe1a2e9902f52dc8006099c45d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "649857313" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1126794995" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
