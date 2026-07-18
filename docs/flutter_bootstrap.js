0a62d4a820c401cef9688d704d19a54386144a1a
manahi ali <shreefjamela@gmail.com>
2026-07-18 14:55:19 +0300

    feat: add terms and settings screen

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"42d3d75a56efe1a2e9902f52dc8006099c45d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3496928624" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1374930133" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
